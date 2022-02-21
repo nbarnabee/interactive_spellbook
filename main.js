61f81d1d5be42e7521b27a93290df7171f209508
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-02-21 15:28:11 +0100

    Commit basic object structure

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 document.getElementById("search-button").addEventListener("click", fetchSpell);
+let newSpell;
 
 function fetchSpell() {
   let searchTerm = document.querySelector("input").value.toLowerCase().split(" ").join("-");
@@ -7,14 +8,32 @@ function fetchSpell() {
   fetch(`https://www.dnd5eapi.co/api/spells/${searchTerm}`)
   .then(res => res.json())
   .then (data => {
-    console.log(data)
+    console.log(data);
+    newSpell = SpellMaker(data);
   })
   .catch(err => console.log(`Error: ${err}`))
   .finally(document.querySelector("input").value = "");
 }
+
+function SpellMaker(input) {
+  return {
+    name: input.name,
+    school: input.school.name,
+    level: input.level,
+    castTime: input.casting_time, 
+    range: input.range,
+    components: input.components,
+    materialComp: input.material,
+    duration: input.duration,
+    concentration: input.concentration,
+    desc: input.desc,
+    desc_higher: input.higher_level, 
+  }
+}
+
 /* 
 Components of a D&D 5thE spell:
-Name
+Name  
 Level
 School
 casting time
